de7ced0588b090d53b57f5c72bdfb92294784edb
4LAR <nikitstolarov@gmail.com>
2022-04-05 18:55:29 +0300

    Начал делать 7 лабу

--- 12 вариант/7/main.cpp
+++ 12 вариант/7/main.cpp
@@ -8,16 +8,25 @@
 */
 
 #include <iostream>
-#include <cmath>
-#include "lib.h"
-
 using namespace std;
+
+#include "libs/lib.h"
+#include <cmath>
+#include <time.h>
+
+// проверка ввода
+#include "libs/simple_char.h"
+#include "libs/input_validation.h"
 
 int main() {
 	// смена кодировки
   system("chcp 65001");
 
+  // очистка терминала
+  clear_scr();
 
+  cout << "Введите строку: ";
+  char_str = get_string(&len);
 
 	return 0;
 }
