5e5d5e059ebcd53c94b0e30ca0ac205082dd265f
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-04-05 22:21:16 +0300

    7 лаба

--- 12 вариант/7/main.cpp
+++ 12 вариант/7/main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 #include "libs/lib.h"
 #include <cmath>
 #include <time.h>
+#include <Windows.h>
 
 // проверка ввода
 #include "libs/simple_char.h"
@@ -25,8 +26,72 @@ int main() {
   // очистка терминала
   clear_scr();
 
-  cout << "Введите строку: ";
-  char_str = get_string(&len);
+  int len;
+  char *char_str;
+
+  // ввод строки
+  while (true) {
+    cout << "Введите строку: ";
+    char_str = get_string(&len);
+
+    if (len > 0) {
+      break;
+    } else {
+      cout << "Вы ввели пустую строку." << endl;
+    }
+  }
+
+  // вывод длинны текущей строки
+  draw_line(20);
+  cout << "Длинна введённой строки: " << len << endl;
+  draw_line(20);
+
+  // ввод длинны новой строки
+  int len_new;
+  while (true) {
+    len_new = read_value("Введите длинну новой строки: ", false, false, false);
+
+    if (len_new <= len) {
+      cout << "Длинна новой строки должна быть больше старой строки (" << len << ")." << endl;
+    } else break;
+  }
+
+  draw_line(20);
+
+  int j = 1;
+  int i = 0;
+  int capacity = 1;
+  // основной код
+  while (len < len_new) {
+    // если встречаем пробел или мы на первом символе
+    if (char_str[i] == ' ' || i == 0) {
+      len++;
+
+      capacity *= 2;
+
+      char_str = (char*) realloc(char_str, len * sizeof(char));
+      for (int k = len; k > i; k--) {
+        char_str[k] = char_str[k-1];
+      }
+      char_str[i] = ' ';
+
+      i += j;
+    }
+
+    // счётсчики
+    i++;
+    if (i >= len) {
+      i = 0;
+      j++;
+    }
+  }
+
+  // выводим новую строку
+  cout << char_str << endl;
+  draw_line(20);
+  cout << "Длинна новой строки: " << len << endl;
+
+  free(char_str);
 
 	return 0;
 }
