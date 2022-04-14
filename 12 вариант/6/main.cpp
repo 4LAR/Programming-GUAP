0a41ccc0d302387bb8e1388baddf98c2de693fda
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-04-14 20:54:10 +0300

    Обновил код для 6 лабы

--- 12 вариант/6/main.cpp
+++ 12 вариант/6/main.cpp
@@ -24,6 +24,7 @@ using namespace std;
 
 // работа с массивами
 #include "libs/array.h"
+#include "clear_arr.h"
 
 int read_size_arr(const char *promt = "") {
   int size;
@@ -43,7 +44,7 @@ int main() {
   system("chcp 65001");
 
   // очистка терминала
-  clear_scr();
+  //clear_scr();
 
   // рандом
   srand(time(NULL));
@@ -68,103 +69,35 @@ int main() {
   }
 
   // ввод значений массива
-  for (y = 0; y < size_y; y++)
-    for (x = 0; x < size_x; x++) {
-      if (RANDOM_NUMS) {
-        arr[y][x] = random_int(-10, 10);
-      } else {
-        cout << "Array[" << y << "][" << x << "] = ";
-        arr[y][x] = read_value("", true, true, false);
-      }
-    }
+  arr = read_double_arr(arr, size_x, size_y, RANDOM_NUMS);
 
   draw_line(20);
   draw_float_double_array(arr, size_x, size_y);
   draw_line(20);
 
   // Найти номер первой из строк, содержащих хотя бы один положительный элемент.
-  // строка с положительным элементом
-  bool find = false;
-  for (y = 0; y < size_y; y++) {
-    for (x = 0; x < size_x; x++) {
-      if (arr[y][x] > 0) {
-        cout << "Индекс первой строки содержащей положительный элемент: " << y << endl;
-        find = true;
-        break;
-      }
-    }
-    if (find) break;
-  }
-
-  if (!find) {
-    cout << "Строк с положительными элементами не существует." << endl;
-  }
+  get_index(arr, size_x, size_y);
 
   draw_line(20);
 
   // Уплотнить заданную матрицу, удаляя из неѐ строки и столбцы, заполненные нулями.
-  // уплотняем матрицу удаляя строки
-  double **new_arr_y = (double**)malloc(sizeof(double*));
-  int size_y_new_arr = 0;
-  bool ok = false;
-  for (y = 0; y < size_y; y++) {
-    ok = false;
-    for (x = 0; x < size_x; x++) {
-      if (arr[y][x] != 0) {
-        ok = true;
-        break;
-      }
-    }
-    if (ok) {
-      new_arr_y = (double**) realloc(new_arr_y, (++size_y_new_arr) * sizeof(double*));
-      new_arr_y[size_y_new_arr-1] = (double*)malloc(size_x * sizeof(double));
 
-      for (int j = 0; j < size_x; j++) {
-        new_arr_y[size_y_new_arr-1][j] = arr[y][j];
-      }
-    }
-  }
-  
-  // для отладки
-  //draw_float_double_array(new_arr_y, size_x, size_y_new_arr); 
-  //draw_line(20);
+  int size_y_new_arr;
+  // уплотняем по высоте
+  arr = clear_height_double_arr(arr, size_x, size_y, &size_y_new_arr);
 
-  double **new_arr_x = (double**)malloc(size_y_new_arr * sizeof(double*));
-  int size_x_new_arr = 0;
-  for (x = 0; x < size_x; x++) {
-    ok = false;
-    for (y = 0; y < size_y_new_arr; y++) {
-      if (new_arr_y[y][x] != 0) {
-        ok = true;
-        break;
-      } 
-    }
-    if (ok) {
-      size_x_new_arr++;
-      for (int j = 0; j < size_y_new_arr; j++) {
-        new_arr_x[j] = (double*) realloc(new_arr_x[j], (size_x_new_arr) * sizeof(double));
-        new_arr_x[j][size_x_new_arr-1] = new_arr_y[j][x];
-      }
-    }
-  }
+  int size_x_new_arr;
+  // уплотняем по ширине
+  arr = clear_width_double_arr(arr, size_x, size_y_new_arr, &size_x_new_arr);
 
-  draw_float_double_array(new_arr_x, size_x_new_arr, size_y_new_arr);
-  
+  // выводим новый массив
+  draw_float_double_array(arr, size_x_new_arr, size_y_new_arr);
+
   // очистка памяти
   for(y = 0; y < size_y; y++) {
       free(arr[y]);
   }
   free(arr);
 
-  for(y = 0; y < size_y_new_arr; y++) {
-      free(new_arr_y[y]);
-  }
-  free(new_arr_y);
-
-  for(y = 0; y < size_y_new_arr; y++) {
-      free(new_arr_x[y]);
-  }
-  free(new_arr_x);
-  
 	return 0;
 }
