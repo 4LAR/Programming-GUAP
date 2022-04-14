0ad747669b74c93a7b47817e331b6cb78d0adf49
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-04-14 21:21:37 +0300

    Обновил код для 8 лабы

--- 12 вариант/8/file.h
+++ 12 вариант/8/file.h
@@ -9,9 +9,9 @@ using namespace std;
 #include <cctype>
 
 char *get_file_name(const char *promt = "", const char *error_promt = "") {
-  
+
   ifstream input_file;
-  
+
   int len;
   char *input_file_name;
 
@@ -37,3 +37,53 @@ char *get_file_name(const char *promt = "", const char *error_promt = "") {
   return input_file_name;
 }
 
+void check_words(char *input_file_name, char *output_file_name, bool data = false) {
+
+  ifstream input_file;
+  ofstream output_file;
+
+  // открываем файлы
+  input_file.open(input_file_name);
+  output_file.open(output_file_name);
+
+  char c;
+  bool space = true;
+  bool state = false;
+
+  if (data) {
+    draw_line(20);
+    cout << "Отладочная информация." << endl;
+    draw_line(20);
+  }
+
+  while (input_file) {
+    c = input_file.get();
+
+    if (!space) { // проверка на пробел перед тире
+      if (isspace(c))
+        space = true;
+      else
+        space = false;
+    } else if (!state) { // проверка на тире после пробела
+      if (c == '-')
+        state = true;
+      else
+        state = false;
+    } else { // записываем предложение в файл
+      if (data) cout << c;
+      output_file << c;
+
+      if (c == '.' || c == '?' || c == '!') { // проверка на конец предложения
+        state = false;
+        space = false;
+        if (data) cout << endl;
+        output_file << endl;
+      }
+    }
+  }
+
+  // закрываем файлы
+  input_file.close();
+  output_file.close();
+
+}
--- 12 вариант/8/main.cpp
+++ 12 вариант/8/main.cpp
@@ -30,57 +30,14 @@ int main() {
   char *input_file_name;
   char *output_file_name;
 
-  ifstream input_file;
-  ofstream output_file;
-
   input_file_name = get_file_name("Имя входного файла: ", "Невозможно прочитать файл. Возможно его не существует.");
   output_file_name = get_file_name("Имя выходного файла: ", "Невозможно записать или создать файл.");
 
-  // открываем файлы
-  input_file.open(input_file_name);
-  output_file.open(output_file_name);
-
-  draw_line(20);
-  cout << "Отладочная информация." << endl;
-  draw_line(20);
-  
-  char c;
-  bool space = true;
-  bool state = false;
-  while (input_file) {
-    c = input_file.get();
-
-    if (!space) { // проверка на пробел перед тире
-      if (isspace(c))
-        space = true;
-      else
-        space = false;
-    } else if (!state) { // проверка на тире после пробела
-      if (c == '-')
-        state = true;
-      else
-        state = false;
-    } else { // записываем предложение в файл
-      cout << c;
-      output_file << c;
-
-      if (c == '.' || c == '?' || c == '!') { // проверка на конец предложения
-        state = false;
-        space = false;
-
-        cout << endl;
-        output_file << endl;
-      }
-    }
-  }
+  check_words(input_file_name, output_file_name, true);
 
   draw_line(20);
 
   cout << "В файл " << output_file_name << " был успешно записан результат." << endl;
-
-  // закрываем файлы
-  input_file.close();
-  output_file.close();
 
   return 0;
 }
