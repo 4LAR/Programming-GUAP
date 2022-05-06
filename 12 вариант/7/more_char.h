4b37ad039403781daf2c0e9b449f41ccae627695
4LAR <nikitstolarov@gmail.com>
2022-05-06 16:14:19 +0300

    блоксхемы

--- 12 вариант/7/more_char.h
+++ 12 вариант/7/more_char.h
@@ -1,17 +1,14 @@
-#include <iostream>
+  #include <iostream>
 using namespace std;
 
 char *add_space_char(char *char_str, int *len, int len_new) {
   int j = 1;
   int i = 0;
-  int capacity = 1;
   // основной код
   while ((*len) < len_new) {
     // если встречаем пробел или мы на первом символе
     if (char_str[i] == ' ' || i == 0) {
       (*len)++;
-
-      capacity *= 2;
 
       char_str = (char*) realloc(char_str, (*len+1) * sizeof(char));
       for (int k = (*len); k > i; k--) {
