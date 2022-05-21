dae8dd02317cde3dc998e9580bcb41121361706f
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-05-21 11:08:10 +0300

    Отчёт

--- 8 вариант/6/arr.h
+++ 8 вариант/6/arr.h
@@ -21,7 +21,7 @@ double get_summ(double **arr, int size_x, int size_y) {
         summ += arr[y][x];
       }
     }
-    
+
   }
 
   return summ;
@@ -30,7 +30,7 @@ double get_summ(double **arr, int size_x, int size_y) {
 // создание характиристики
 double *get_info_arr(double **arr, int size_x, int size_y) {
   double *info = (double*)malloc(size_x * sizeof(double));
-  
+  for (int i = 0; i < size_x; i++) info[i] = 0;
   for (int x = 0; x < size_x; x++) {
     for (int y = 0; y < size_y; y++) {
       if ((arr[y][x] < 0) && ((int)arr[y][x] % 2 != 0)) {
