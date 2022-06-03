43f7e8aad8a5a8738e15d9479768778db89d9185
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-06-03 22:00:49 +0300

    7 лаба

--- 11 вариант/7/main.cpp
+++ 11 вариант/7/main.cpp
@@ -16,7 +16,6 @@ using namespace std;
 
 // проверка ввода
 #include "libs/simple_char.h"
-#include "libs/input_validation.h"
 
 #include "more_char.h"
 
@@ -30,19 +29,27 @@ int main() {
   int len, other_len;
   char *char_str, *other_char_str;
 
+  // вводим строки
   char_str = check_char(&len, "Введите строку: ");
   other_char_str = check_char(&other_len, "Введите подстроку: ");
 
   draw_line(20);
 
-  //
+  // вызов функции для поиска строки
+  int last_index = get_last(char_str, len, other_char_str, other_len);
+  if (last_index != -1) {
+    cout << "Начало найденной строки (индекс): " << last_index << endl;
 
+    // вывод строки (в скобках будет показана найденная строка)
+    for (int i = 0; i < len; i++) {
+      cout << ((i == last_index)? "[": "") << char_str[i] << ((i == last_index + (other_len - 1))? "]": "");
+    }
+    cout << endl;
+  } else {
+    cout << "Ничего не найдено" << endl;
+  }
 
-  // выводим новую строку
-  cout << char_str << endl;
-  draw_line(20);
-  cout << "Длинна новой строки: " << len << endl;
-
+  // очищаем память
   free(char_str);
   free(other_char_str);
 
--- 11 вариант/7/more_char.h
+++ 11 вариант/7/more_char.h
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// функция для нахождения строки
 int get_last(char *str, int len, char *other_char_str, int other_len) {
-  
+
+  int get_index = other_len - 1;
+
+  // обратный цикл по всей строке
+  for (int i = (len - 1); i > 0; i--) {
+    if (str[i] == other_char_str[get_index]) {
+      get_index--;
+    } else {
+      get_index = other_len - 1;
+    }
+
+    // если нашли строку, то возвращаем её индекс
+    if (get_index < 0) return i;
+  }
+
+  // если не нашли, возрвщаем -1
+  return -1;
 }
