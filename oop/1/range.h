957859116ec1aa9d3046284b183647c453cf47c0
4LAR <nikitstolarov@gmail.com>
2022-09-12 19:53:16 +0300

    Сдал

--- oop/1/range.h
+++ oop/1/range.h
@@ -3,22 +3,26 @@ using namespace std;
 
 class Range{
   public:
-    void rangedraw() {
-      cout << "Левая граница: " << left << endl;
-      cout << "Правая граница: " << right << endl;
-    }
-
-    void rangeset(double set_left, double set_right) {
-      left = set_left;
-      right = set_right;          
-    }
-
-    bool rangecheck(double num) {
-      return (left <= num) && (num < right);
-    }
-
+    void rangedraw();
+    void rangeset(double set_left, double set_right);
+    bool rangecheck(double num);
+    
   private:
     double left = 0;
     double right = 0;
 
 };
+
+void Range::rangedraw() {
+  cout << "Левая граница: " << left << endl;
+  cout << "Правая граница: " << right << endl;
+}
+
+void Range::rangeset(double set_left, double set_right) {
+  left = set_left;
+  right = set_right;  
+}
+
+bool Range::rangecheck(double num) {
+  return (left <= num) && (num < right);
+}
