679fa64f2104bcada1f7d61559f5b85743baa64b
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-10-06 15:32:22 +0300

    кто

--- SAOD/2/structs.h
+++ SAOD/2/structs.h
@@ -37,3 +37,70 @@ void draw_list(list *number_list) {
     number_list = number_list -> next;
   }
 }
+
+void draw_func(list *tmp, const char *promt = "") {
+  cout << promt;
+  bool first = true;
+  while (tmp != NULL) {
+    if (tmp -> a != 0) {
+      if (!first)
+        cout << " + ";
+      else first = false;
+
+
+      cout << tmp -> a;
+
+      if (tmp -> n != 0) cout << "x^" << tmp -> n;
+
+    }
+
+    tmp = tmp -> next;
+  }
+  cout << endl;
+}
+
+int get_size_list(list *tmp) {
+  int size = 0;
+  while (tmp != NULL) {
+    size++;
+    tmp = tmp -> next;
+  }
+  return size;
+}
+
+list *merge_func(list *a, list *b) {
+  list *tmp = NULL;
+
+  int size_a = get_size_list(a);
+  int size_b = get_size_list(b);
+
+  if (size_b > size_a) {
+    list *buf = a;
+    a = b;
+    b = buf;
+  }
+
+  while (a != NULL) {
+    if (b != NULL && ((b -> n) == (a -> n))) {
+
+      if (tmp == NULL) {
+        tmp = create(a -> n, (a -> a) + (b -> a));
+      } else {
+        add_element_end(a -> n, (a -> a) + (b -> a), tmp);
+      }
+      b = b -> next;
+
+    } else {
+
+      if (tmp == NULL) {
+        tmp = create(a -> n, a -> a);
+      } else {
+        add_element_end(a -> n, a -> a, tmp);
+      }
+    }
+
+    a = a -> next;
+  }
+
+  return tmp;
+}
