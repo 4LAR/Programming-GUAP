8582371fda5604e19cd0dc411ea6e49c789d212a
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-11-10 22:16:47 +0300

    5 лаба

--- SAOD/5/main.cpp
+++ SAOD/5/main.cpp
@@ -1,4 +1,20 @@
 /*
+  27 вариант
+
+  Найти k-ое по порядку число среди элементов массива
+    Расческой
+
+    Использовать неупорядоченный массив A, содержащий n
+  целочисленных элементов. Величина n определяется по согласованию с
+  преподавателем. Дополнительно в программе должны быть реализованы
+  следующие функции:
+
+    1) Поиск элемента либо по его порядковой позиции, либо по его
+    содержимому;
+    2) Добавление/удаление элемента с последующей пересортировкой
+    последовательности;
+    3) В программе должен быть реализован подсчет количества
+    сравнений и перестановок, при осуществлении сортировки.
 
 */
 
@@ -14,11 +30,66 @@ using namespace std;
 #include "libs/simple_char.h"
 #include "libs/input_validation.h"
 
+#include "libs/array.h"
+
+#include "sort.h"
+
+int menu() {
+  int id;
+  while (true) {
+    draw_line(30);
+    cout << "1) добавить элемент в массив" << endl;
+    cout << "2) удалить элемент из массива" << endl;
+    cout << "3) вывести массив" << endl;
+    cout << "4) сортировать массив" << endl;
+    cout << "0) Выход" << endl;
+    id = read_value(" >>> ", false, false, false);
+    if (id >= 0 && id <= 4) {
+      return id;
+    } else {
+      cout << "Этого нет в меню" << endl;
+    }
+  }
+}
+
 int main() {
   // смена кодировки
   system("chcp 65001");
+  draw_line();
 
-  
+  int size = read_value("Разсер массива: ", false, true, false);
 
+  Array array(size, true, true);
+
+  int menu_i;
+  while (true) {
+    menu_i = menu();
+
+    switch (menu_i) {
+    case (0):
+      return 0;
+      break;
+
+    case (1):
+      array.add(read_value("Добавляемый элемент: ", false, false, false));
+      break;
+
+    case (2): {
+      int returned = array.pop(read_value("Id удаляемого элемена: ", false, false, false));
+      cout << "Удалённый элемент: " << returned << endl;
+      break;
+    }
+    
+    case (3):
+      array.draw("Массив: ");
+      break;
+
+    case (4):
+      array.sort_arr();
+      break;
+
+    }
+
+  }
   return 0;
 }
---
+++ SAOD/5/sort.h
@@ -0,0 +1,123 @@
+
+#include <iostream>
+using namespace std;
+
+#define rand_min -10
+#define rand_max 10
+
+class Array {
+public:
+  Array(int, bool, bool);
+  ~Array();
+
+  int* sort_arr();
+  int* generator();
+  void add(int);
+  int pop(int);
+  void draw(const char* promt);
+
+private:
+  int* arr;
+
+  int size;
+  bool auto_sort;
+  bool random;
+
+};
+
+// конструктор
+Array::Array(int Size, bool Auto_sort = true, bool Random = true) {
+  size = Size;
+  auto_sort = Auto_sort;
+  random = Random;
+
+  arr = (int*)malloc(size * sizeof(int));
+  if (random) {
+    generator();
+  } else {
+    for (int i = 0; i < size; i++) {
+      cout << "Array[" << i << "] = ";
+      arr[i] = read_value("", false, false, false);
+    }
+  }
+
+  draw("Изначальный массив: ");
+
+  if (auto_sort) sort_arr();
+}
+
+// деструктор
+Array::~Array() {
+  free(arr);
+}
+
+// вывод массива
+void Array::draw(const char* promt = "") {
+  cout << promt;
+  for (int i = 0; i < size; i++) cout << arr[i] << " ";
+  cout << endl;
+}
+
+// добавление элемента в массив
+void Array::add(int x) {
+  arr = (int*)realloc(arr, ++size * sizeof(int));
+
+  arr[size - 1] = x;
+
+  if (auto_sort) sort_arr();
+}
+
+// удаление элемента в массиве
+int Array::pop(int x) {
+
+  int returned = arr[x];
+
+  int* old_arr = (int*)malloc(size * sizeof(int));
+  old_arr = arr;
+  arr = (int*)malloc((size - 1) * sizeof(int));
+
+  int i, j = 0;
+  for (i = 0; i < size; i++) {
+    if (i != x) arr[j++] = old_arr[i];
+  }
+
+  size--;
+  free(old_arr);
+
+  if (auto_sort) sort_arr();
+
+  return returned;
+}
+
+// заполнение массива случайными числами
+int* Array::generator() {
+  for (int i = 0; i < size; i++) {
+    arr[i] = random_int(rand_min, rand_max);
+  }
+  return arr;
+}
+
+// сортировка расчёской
+int* Array::sort_arr() {
+  int if_count = 0; // количество сравнений
+  int swap_count = 0; // количество перестановок
+
+  const double factor = 1.247; // Фактор уменьшения
+  double step = size - 1;
+
+  while (step >= 1) {
+    for (int i = 0; i + step < size; ++i) {
+      if_count++;
+      if (arr[i] > arr[(int)(i + step)]) {
+        swap_count++;
+        swap(arr[i], arr[(int)(i + step)]);
+      }
+    }
+    step /= factor;
+  }
+
+  cout << "[Сортировка] " << if_count << " сравнений, " << swap_count << " перестановок" << endl;
+  draw("[Сортировка]: ");
+
+  return arr;
+}
