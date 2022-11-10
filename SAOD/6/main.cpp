297b957dc635146c7021734eb2c8f30d31e03d73
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-11-10 22:23:25 +0300

    начал делать 6 лабу

---
+++ SAOD/6/main.cpp
@@ -0,0 +1,25 @@
+/*
+  3 вариант (26 % 24 + 1)
+
+  
+
+*/
+
+#include <iostream>
+using namespace std;
+
+#include "libs/lib.h"
+#include <cmath>
+#include <time.h>
+#include <iomanip>
+
+// проверка ввода
+#include "libs/simple_char.h"
+#include "libs/input_validation.h"
+
+int main() {
+  // смена кодировки
+  system("chcp 65001");
+
+  return 0;
+}
