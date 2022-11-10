fbddc0c4d24a9f158f987130bfc7fe82665e6962
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-11-10 20:07:27 +0300

    начал делать 5

---
+++ SAOD/5/main.cpp
@@ -0,0 +1,24 @@
+/*
+
+*/
+
+#include <iostream>
+using namespace std;
+
+#include "libs/lib.h"
+#include <cmath>
+#include <time.h>
+#include <iomanip>
+
+// проверка ввода
+#include "libs/simple_char.h"
+#include "libs/input_validation.h"
+
+int main() {
+  // смена кодировки
+  system("chcp 65001");
+
+  
+
+  return 0;
+}
