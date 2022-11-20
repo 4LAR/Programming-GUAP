1a1b09fea9890cbc3ffa09c51281bcdcfd6a9f1e
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-11-20 18:09:20 +0300

    6 лаба ооп

---
+++ oop/6/arr_vec.h
@@ -0,0 +1,72 @@
+
+
+#include <iostream>
+using namespace std;
+
+#include <iomanip>
+#include <vector>
+
+#define rand_min -50 // m1
+#define rand_max 50  // m2
+//#define separator_show " " //
+
+// класс реализующий работу с вектором
+class Arr_vec {
+public:
+  Arr_vec(int);
+  ~Arr_vec();
+
+  void generator(int);
+  void show(const char* separator);
+  void chenage_on_zero();
+  bool swap_els(int);
+
+private:
+  vector <int> my_vector;
+};
+
+// конструктор
+Arr_vec::Arr_vec(int Size) {
+  // my_vector.reserve(Size);
+  generator(Size);
+}
+
+// деструктор
+Arr_vec::~Arr_vec() {
+
+}
+
+// метод заполняющий вектор случайными числами
+void Arr_vec::generator(int Size) {
+  for (int i = 0; i < Size; i++) {
+    my_vector.push_back(random_int(rand_min, rand_max));
+  }
+}
+
+// вывод массива на экран
+void Arr_vec::show(const char* separator = " ") {
+  for (int i = 0; i < my_vector.size(); i++) {
+    cout << setw(4) << my_vector.at(i) << separator;
+  }
+  cout << endl;
+}
+
+// замена эелементов массива (всё что меньше 10 заменить на 0)
+void Arr_vec::chenage_on_zero() {
+  for (int i = 0; i < my_vector.size(); i++) {
+    if (my_vector.at(i) < 10)
+      my_vector.at(i) = 0;
+  }
+}
+
+// зеркально обменять 2 первых элемента массива с 2 последними
+bool Arr_vec::swap_els(int count = 2) {
+  if (count * 2 > my_vector.size())
+    return false;
+
+  for (int i = 0; i < count; i++) {
+    swap(my_vector.at(i), my_vector.at(my_vector.size() - i - 1));
+  }
+
+  return true;
+}
