8af617ae6b0ed39824fb2825459ea758d263dfd9
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2023-02-27 21:40:40 +0300

    каво

--- computational_mathematics/1/main.cpp
+++ computational_mathematics/1/main.cpp
@@ -22,6 +22,8 @@ class Method_HORD {
 public:
   Method_HORD(double, double, double, double);
   double func(double);
+  double derivative_func(double);
+  double double_derivative_func(double);
   double find(double, double);
 
 private:
@@ -43,11 +45,6 @@ Method_HORD::Method_HORD(double _a, double _b, double _c, double _epsilon) {
   cout << "  E = " << epsilon << endl;
 }
 
-// функция
-double Method_HORD::func(double x) {
-  return (a / x) + b * exp(c * x);
-}
-
 // double Method_HORD::find(double min, double max) {
 //   while (fabs(max - min) > epsilon) {
 //     min = max - (max - min) * func(max) / (func(max) - func(min));
@@ -59,22 +56,61 @@ double Method_HORD::func(double x) {
 // }
 
 // нахождение корня
-double Method_HORD::find(double a, double b) {
-  double t;
+// double Method_HORD::find(double a, double b) {
+//   double t;
+//
+//   while (fabs(b - a) >= epsilon) {
+//     t = a + (func(b) * (b - a)) / (func(b) - func(a));
+//
+//     if (func(a) * func(t) < 0) {
+//       b = t;
+//     } else if (func(t) * func(b) < 0) {
+//       a = t;
+//     } else
+//       return t;
+//   }
+//
+//   return t;
+// }
 
-  while (fabs(b - a) >= epsilon) {
-    t = a + (func(b) * (b - a)) / (func(b) - func(a));
-
-    if (func(a) * func(t) < 0) {
-      b = t;
-    } else if (func(t) * func(b) < 0) {
-      a = t;
-    } else
-      return t;
-  }
-
-  return t;
+// функция
+double Method_HORD::func(double x) {
+  return (a / x) + b * exp(c * x);
 }
+
+// функция
+double Method_HORD::derivative_func(double x) {
+  return (b * c) * exp(c * x) - (a / pow(x, 2));
+}
+
+// функция
+double Method_HORD::double_derivative_func(double x) {
+  return (b * c * c) * exp(c * x) + (a * a) / pow(x, 3);
+}
+
+
+double Method_HORD::find(double a, double b) {
+  int iterCount = 0;
+  double d, c;
+	while (abs(abs(b) - abs(a)) > 2 * epsilon) {
+		if (func(a) * double_derivative_func(a) > 0) {
+			d = a - func(a) / derivative_func(a);
+			c = (a * func(b) - b * func(a)) / (func(b) - func(a));
+			a = d;
+			b = c;
+		} else if (func(b) * double_derivative_func(b) > 0) {
+			d = b - func(b) / derivative_func(b);
+			c = (a * func(b) - b * func(a)) / (func(b) - func(a));
+			b = d;
+			a = c;
+		}
+		iterCount++; // узнать кол-во итераций
+    cout << iterCount << " " << a << " " << b << endl;
+	}
+	return (a + b) / 2;
+}
+
+
 
 int main() {
   // смена кодировки
@@ -89,7 +125,7 @@ int main() {
   Method_HORD method_HORD(a, b, c, epsilon);
 
   // рассчёт
-  cout << method_HORD.find(-5, 5) << endl;
+  cout << method_HORD.find(0.1, 300) << endl;
 
   return 0;
 }
