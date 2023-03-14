3128bd5562811ed3adb003a2afa21a730626be81
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2023-03-15 01:04:19 +0300

    3 лаба

---
+++ oop_qt/3/mainwindow.cpp
@@ -0,0 +1,62 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QPushButton>
+#include <QString>
+#include <QStringListModel>
+#include <QDebug>
+#include <QtMath>
+
+#define length_materials 3
+
+int materials[length_materials] = {
+//    {"Флис", "200"},
+//    {"Хлопок", "400"},
+//    {"Лён", "500"}
+    200,
+    400,
+    500
+};
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setFixedSize(this->width(), this->height());
+
+    ui->lineEdit_4->setText(QString::number(materials[0]));
+    ui->lineEdit->setText(QString::number(1));
+    ui->lineEdit_2->setText(QString::number(1));
+
+    connect(
+        ui->pushButton,
+        &QPushButton::released,
+        this,
+        [this]{calculate(); });
+
+}
+
+void MainWindow::calculate() {
+    int current_index = ui->comboBox->currentIndex();
+//    ui->lineEdit;
+//    ui->lineEdit_2;
+//    ui->lineEdit_4;
+    //.toDouble()
+    double width = ui->lineEdit->text().toDouble();
+    double height = ui->lineEdit_2->text().toDouble();
+    ui->lineEdit_3->setText(
+       QString::number(width * height * materials[current_index])
+    );
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_comboBox_currentIndexChanged(int index)
+{
+    ui->lineEdit_4->setText(QString::number(materials[index]));
+}
