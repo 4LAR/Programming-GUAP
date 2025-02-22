9a0ffe19c3f1059208fb2cb40cb72772f9a6dc36
4LAR <nikitstolarov@gmail.com>
2025-02-22 11:46:32 +0300

    олд

--- Processing of experimental data/1/main.py
+++ Processing of experimental data/1/main.py
@@ -5,18 +5,43 @@ import matplotlib.pyplot as plt
 # Чтение данных из файла
 filename = 'data.txt'
 data = np.loadtxt(filename)
+length_data = len(data)
 
 # 1. Оценка математического ожидания
 mean_estimate = np.mean(data)
 print(f"Оценка математического ожидания: {mean_estimate:.4f}")
+
+def interv(data_):
+    result = [0 for _ in range(length_data)]
+    for i in range(length_data):
+        result += (data[i] - mean_estimate) ** 2
+
+    disp = (result / (length_data - 1))[0]
+
+    sko = np.sqrt(disp)
+
+    inter = [mean_estimate - 2 * sko, mean_estimate + 2 * sko]
+
+    return inter, result, sko, disp
+
+inter, arr, sko, disp = interv(data)
+
+print("Интервал", inter)
+print("Массив", arr)
+print("СКО",  sko)
+print("Диперсия", disp)
 
 # 2. Построение 95%-го доверительного интервала
 n = len(data)
 std_dev = np.std(data, ddof=1)  # Стандартное отклонение с поправкой на выборку
 z_value = stats.norm.ppf(0.975)  # z-критерий для 95% доверительного интервала
 
+# !!!!!!!! СКО - я хз что это но это налдо посчитать
+# !!!!!!!!! 11 интервалов должно быть т.к. на входе 12
 margin_of_error = z_value * (std_dev / np.sqrt(n))
+print(margin_of_error)
 confidence_interval = (mean_estimate - margin_of_error, mean_estimate + margin_of_error)
+print(confidence_interval)
 print(f"95%-й доверительный интервал: {confidence_interval}")
 
 # 3. Отсечение аномальных наблюдений
